73ec65f34ea8b59f92ce1de5d3b1d56d247af06e
mohammedadamdev <110044593+mohammedadamdev@users.noreply.github.com>
2023-05-12 04:31:30 +0530

    functionality for the routes

--- scheduled_tweets/app/controllers/main_controller.rb
+++ scheduled_tweets/app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
   def index
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
     flash.now[:notice] = "Logged in successfully"
     flash.now[:alert] = "Invalid email or password"
   end
--- scheduled_tweets/config/routes.rb
+++ scheduled_tweets/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
  #GET /about
   get "about", to: "about#index"
 
+  get "sign_up", to: "registration#new"
+
+  post "sign_up", to: "registration#create"
+
   # get "/", to: "main#index" #Method one
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
