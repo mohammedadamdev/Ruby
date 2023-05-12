c5f007bad3c0fab9ce90d52469e4b9a6a9d1f6da
mohammedadamdev <110044593+mohammedadamdev@users.noreply.github.com>
2023-05-12 22:35:06 +0530

    Session cookies added to the main.rb file

--- scheduled_tweets/app/controllers/main_controller.rb
+++ scheduled_tweets/app/controllers/main_controller.rb
@@ -1,9 +1,9 @@
 class MainController < ApplicationController
   def index
     if session[:user_id]
-      @user = User.find(session[:user_id])
+      @user = User.find_by(id: session[:user_id])
     end
-    flash.now[:notice] = "Logged in successfully"
-    flash.now[:alert] = "Invalid email or password"
+    # flash.now[:notice] = "Logged in successfully"
+    # flash.now[:alert] = "Invalid email or password"
   end
 end
